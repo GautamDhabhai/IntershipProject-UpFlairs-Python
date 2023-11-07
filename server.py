4d6837481c76a90f49d331160d3cd503bd307dd6
GautamDhabhai <109916842+GautamDhabhai@users.noreply.github.com>
2023-11-07 23:27:44 +0530

    comments added

--- server.py
+++ server.py
@@ -3,12 +3,15 @@ import queue
 import threading
 import os
 
+#a queue and a list for storing messages and client address
 messages = queue.Queue()
 clients = []
 
+#binding the server
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("192.168.29.169", 1))
 
+#function to save chat history in a txt file
 def save_chat_history(message, address):
     with open("chat_history.txt", "a") as chat_file:
         chat_file.write(f"{address}: {message.decode()}\n")
@@ -17,16 +20,19 @@ if not os.path.exists("chat_history.txt"):
     with open("chat_history.txt", "w"):
         pass
 
+#function to recieve messages in the server and put in into message queue
 def recieve():
     while 5 < 6:
         try:
             message, address = server.recvfrom(1024)
             messages.put((message, address))
-
+            
+            #calling save_chat
             save_chat_history(message, address)
         except:
             pass
 
+#function for sending message to all of the clients
 def broadcast():
     while 4 < 5:
         while messages.empty() == False:
@@ -46,6 +52,7 @@ def broadcast():
                 except:
                     clients.remove(client)
 
+#threading of recieve and breadcast started
 t1 = threading.Thread(target=recieve)
 t2 = threading.Thread(target=broadcast)
 
