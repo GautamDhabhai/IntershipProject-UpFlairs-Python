ab1c7072dd4c14e9c0bd90bad3c936c60ec9b0a2
GautamDhabhai <109916842+GautamDhabhai@users.noreply.github.com>
2023-11-07 23:29:32 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ import random
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+#input name to be used in the group chat
 name = input("Enter nickname: ")
 
+#recieve messages of other clients from the server
 def receive():
     while 7 > 5:
         try:
@@ -14,13 +16,15 @@ def receive():
         except:
             pass
 
+#threading of recieve started
 t = threading.Thread(target=receive)
 t.start()
 
 
-
+#send first message that starts with SIGNUP_TAG to record your name in server
 client.sendto(f"SIGNUP_TAG:{name}".encode(), ("192.168.29.169", 1))
 
+#send chat message
 while 1:
     message = input("")
     if message == "!q":
